8290bd36c0d2928e3210fca605ec06058d7d81e0
Xavier Romero <xromero@vodafone.es>
2011-08-25 21:17:20 +0200

    Modificaciones 25/08/2011 - Completar Botones pantalla Numérica, empezar botones pantalla Funciones.

--- Mando 4G/SecondViewController.h
+++ Mando 4G/SecondViewController.h
@@ -15,6 +15,31 @@
 - (IBAction)boto_Ok:(id)sender;
 - (IBAction)boto_Config:(id)sender;
 - (IBAction)boto_Info:(id)sender;
+- (IBAction)boto_Up:(id)sender;
+- (IBAction)boto_Down:(id)sender;
+- (IBAction)boto_Left:(id)sender;
+- (IBAction)boto_Right:(id)sender;
+- (IBAction)boto_Opera:(id)sender;
+- (IBAction)boto_Usb:(id)sender;
+- (IBAction)boto_Disc:(id)sender;
+- (IBAction)boto_Red:(id)sender;
+- (IBAction)boto_Widgets:(id)sender;
+- (IBAction)boto_CatchTv:(id)sender;
+- (IBAction)boto_Grab:(id)sender;
+- (IBAction)boto_Epg:(id)sender;
+- (IBAction)boto_Fav:(id)sender;
+- (IBAction)boto_Mute:(id)sender;
+
+- (IBAction)boto_Zero:(id)sender;
+- (IBAction)boto_One:(id)sender;
+- (IBAction)boto_Two:(id)sender;
+- (IBAction)boto_Three:(id)sender;
+- (IBAction)boto_Four:(id)sender;
+- (IBAction)boto_Five:(id)sender;
+- (IBAction)boto_Six:(id)sender;
+- (IBAction)boto_Seven:(id)sender;
+- (IBAction)boto_Eight:(id)sender;
+- (IBAction)boto_Nine:(id)sender;
 
 // Test
 @end
--- Mando 4G/SecondViewController.m
+++ Mando 4G/SecondViewController.m
@@ -42,6 +42,8 @@
 }
 
 - (IBAction)boto_Power:(id)sender {
+    UInt8 buf[] = CMD_POWER;
+    NSString *textSortida = (NSString*) sendCommand(buf);
 }
 
 - (IBAction)boto_Menu:(id)sender {
@@ -66,6 +68,126 @@
 
 - (IBAction)boto_Info:(id)sender {
     UInt8 buf[] = "K|2";
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Up:(id)sender {
+    UInt8 buf[] = CMD_UP;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Down:(id)sender {
+    UInt8 buf[] = CMD_DOWN;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Left:(id)sender {
+    UInt8 buf[] = CMD_LEFT;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Right:(id)sender {
+    UInt8 buf[] = CMD_RIGHT;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Opera:(id)sender {
+    UInt8 buf[] = CMD_OPERA;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Usb:(id)sender {
+    UInt8 buf[] = CMD_USB;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Disc:(id)sender {
+    UInt8 buf[] = CMD_HDD;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Red:(id)sender {
+    UInt8 buf[] = CMD_RED;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Widgets:(id)sender {
+    UInt8 buf[] = CMD_OKI_WIDGETSTV;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_CatchTv:(id)sender {
+    UInt8 buf[] = CMD_OKI_CATCHTV;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Grab:(id)sender {
+    UInt8 buf[] = CMD_GRAB;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Epg:(id)sender {
+    UInt8 buf[] = CMD_EPG;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Fav:(id)sender {
+    UInt8 buf[] = CMD_OKI_FAVORITE;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Mute:(id)sender {
+    UInt8 buf[] = CMD_MUTE;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Zero:(id)sender {
+    UInt8 buf[] = CMD_ZERO;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_One:(id)sender {
+    UInt8 buf[] = CMD_ONE;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Two:(id)sender {
+    UInt8 buf[] = CMD_TWO;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Three:(id)sender {
+    UInt8 buf[] = CMD_THREE;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Four:(id)sender {
+    UInt8 buf[] = CMD_FOUR;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Five:(id)sender {
+    UInt8 buf[] = CMD_FIVE;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Six:(id)sender {
+    UInt8 buf[] = CMD_SIX;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Seven:(id)sender {
+    UInt8 buf[] = CMD_SEVEN;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Eight:(id)sender {
+    UInt8 buf[] = CMD_EIGHT;
+    NSString *textSortida = (NSString*) sendCommand(buf);
+}
+
+- (IBAction)boto_Nine:(id)sender {
+    UInt8 buf[] = CMD_NINE;
     NSString *textSortida = (NSString*) sendCommand(buf);
 }
 @end
